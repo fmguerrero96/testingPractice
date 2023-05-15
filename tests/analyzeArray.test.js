2149884911f859e82deafdd9f99755273f695bd0
Misael Guerrero <mguerrerowest@gmail.com>
2023-05-15 10:45:12 -0400

    create test for analyzeArray

---
+++ tests/analyzeArray.test.js
@@ -0,0 +1,6 @@
+const analyzeArray = require('../code/analyzeArray')
+
+test('analyze the given array', () => {
+    expect(analyzeArray([1,8,3,4,2,6])).toMatchObject
+    ({ average: 4, min: 1, max: 8, length: 6 })
+})
